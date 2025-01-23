4fba49cd14cc66ec687e2c30833641e6df11e823
laurasfelix <laurasfelix@hotmail.com.br>
2025-01-23 01:12:30 -0800

    weird

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Enables static export
-  trailingSlash: true, // Ensures proper routing for GitHub Pages
-  assetPrefix: './', // Adjusts asset paths for GitHub Pages
-  images: {
-    unoptimized: true, // Disables Next.js Image Optimization
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
